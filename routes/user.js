66fa41eb14aa358f73a349cc1587eaba4c3a07d3
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-02-09 09:06:50 +0200

    Add /users controller functions

--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,37 @@ const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
 
-// Manage endpoints
+// Index (/users)
 router.route('/')
     .get(usersController.getAllUsers)
     .post(usersController.createNewUser)
 
+// User-specific endpoints
 router.route('/:id')
     .get(usersController.getUserById)
     .patch(usersController.updateUser)
     .delete(usersController.deleteUser)
 
+// User transactions
 router.route('/:id/transactions')
     .get(usersController.getAllUserTransactions)
     .post(usersController.createTransaction)
+
+router.route('/:id/transactions/:trId')
+    .get(usersController.getTransactionById)
+    .patch(usersController.updateTransaction)
+    .delete(usersController.deleteTransaction)
+
+// User accounts
+router.route('/:id/accounts')
+    .get(usersController.getAllUserAccounts)
+    .post(usersController.createNewAccount)
+
+router.route('/:id/accounts/:id')
+    .get(usersController.getAccountById)
+    .patch(usersController.updateAccount)
+    .delete(usersController.deleteAccount)
+
 
 
 module.exports = router
